eb2c49e3cae5d6bdbe065624b60ad687aeea776b
rickertc <u1366385@umail.utah.edu>
2021-11-19 13:42:11 -0800

    Grocery Store

---
+++ index.js
@@ -0,0 +1,11 @@
+var shopper = {
+    firstName: "Fred",
+    lastName: "Meyer",
+    age: 100,
+    alive: true,
+    grocerycart: ["Tomatoes", "Peaches", "Cherries", "Toothpaste"],
+    fullname: function() {return this.firstName + " " + this.lastName;}
+    }
+    
+console.log(shopper)
+
